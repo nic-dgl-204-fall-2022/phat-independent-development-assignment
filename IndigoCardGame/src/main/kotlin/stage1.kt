1b63e6b4ef293a075d4582fdd93cdee684d6b88f
phat@minim1 <phat.tranducvn@gmail.com>
2022-11-01 17:18:19 -0700

    Refactor: Create functions for each feature

--- IndigoCardGame/src/main/kotlin/stage1.kt
+++ IndigoCardGame/src/main/kotlin/stage1.kt
@@ -1,64 +1,65 @@
+val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+val suits = listOf("♦", "♥", "♠", "♣")
+var deckCards =  getFullCards()
+
+fun get(cards: MutableList<String>): MutableList<String> {
+    var newCards: MutableList<String> = mutableListOf()
+    println("Number of cards:")
+    try {
+        val numOfCards = readln().toInt()
+        if (numOfCards !in 1..52) {
+            println("Invalid number of cards.")
+            return newCards
+        }
+        // Print removed cards
+        println(cards.slice(0 until numOfCards).joinToString(" "))
+        newCards = cards.drop(numOfCards).toMutableList()
+    }   catch(e: IndexOutOfBoundsException) {
+        println("The remaining cards are insufficient to meet the request.")
+    }   catch(e: NumberFormatException) {
+        println("Invalid number of cards.")
+    }
+
+    return newCards
+}
+
+fun reset(): MutableList<String> {
+    println("Card deck is reset.")
+    return getFullCards()
+}
+
+fun shuffle(cards: MutableList<String>): MutableList<String> {
+    println("Card deck is shuffled.")
+    return cards.shuffled().toMutableList()
+}
+
+fun exit() = println("Bye")
+
+fun showMenu() {
+    println("Choose an action (reset, shuffle, get, exit):")
+
+    when(readlnOrNull()) {
+        "get" -> deckCards = get(deckCards)
+        "reset" -> deckCards = reset()
+        "shuffle" -> deckCards = shuffle(deckCards)
+        "exit" -> return exit()
+        else -> println("Wrong action.")
+    }
+    showMenu()
+}
+
+fun getFullCards(): MutableList<String> {
+    val cards = mutableListOf<String>()
+
+    for (suit in suits) {
+        for (rank in ranks) {
+            cards.add("$rank$suit")
+        }
+    }
+
+    return cards
+}
 
 fun main() {
-    val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
-    val suits = listOf("♦", "♥", "♠", "♣")
-    val deckCards = listOf("K♣", "Q♣", "J♣", "10♣", "9♣", "8♣", "7♣", "6♣", "5♣", "4♣", "3♣", "2♣", "A♣", "K♦", "Q♦", "J♦", "10♦", "9♦", "8♦", "7♦", "6♦", "5♦", "4♦", "3♦", "2♦", "A♦", "K♥", "Q♥", "J♥", "10♥", "9♥", "8♥", "7♥", "6♥", "5♥", "4♥", "3♥", "2♥", "A♥", "K♠", "Q♠", "J♠", "10♠", "9♠", "8♠", "7♠", "6♠", "5♠", "4♠", "3♠", "2♠", "A♠")
-
-    // println(ranks.joinToString(" "))
-    // println(suits.joinToString(" "))
-    // println(deckCards.joinToString(" "))
-
-    var userCards = deckCards
-    var userChoice: String? = null
-
-    do {
-        println("Choose an action (reset, shuffle, get, exit):")
-        userChoice = readln()
-
-        when(userChoice) {
-            "get" -> {
-                println("Number of cards:")
-                try {
-                    val numOfCards = readln().toInt()
-                    if (numOfCards in 1..52) {
-                        if (numOfCards > userCards.size) {
-                            println("The remaining cards are insufficient to meet the request.")
-                        } else {
-                            val removedCards = userCards.slice(0 until numOfCards)
-                            for (card in removedCards) {
-                                if (card == removedCards.last()) {
-                                    println(card)
-                                } else {
-                                    print("$card ")
-                                }
-                            }
-                            userCards = userCards.drop(numOfCards)
-                        }
-                    } else {
-                        println("Invalid number of cards.")
-                    }
-                } catch(e: Exception) {
-                    println("Invalid number of cards.")
-                }
-                userChoice = null
-            }
-            "reset" -> {
-                userCards = deckCards
-                println("Card deck is reset.")
-                userChoice = null
-            }
-            "shuffle" -> {
-                userCards = userCards.shuffled()
-                println("Card deck is shuffled.")
-                userChoice = null
-            }
-            "exit" -> {
-                println("Bye")
-            }
-            else -> {
-                println("Wrong action.")
-                userChoice = null
-            }
-        }
-    } while (userChoice == null)
+    showMenu()
 }
