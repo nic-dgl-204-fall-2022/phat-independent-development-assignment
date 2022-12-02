019a5f1c3bbf716a494bce2bdbaf5d1eaba4f04d
phat@minim1 <phat.tranducvn@gmail.com>
2022-12-01 20:27:40 -0800

    Show pokemon list

--- PokemonLandUI/src/js/pokemon.js
+++ PokemonLandUI/src/js/pokemon.js
@@ -1,13 +1,3 @@
-const backToPokemonModalButton = document.getElementById("back-to-pokemon-modal-btn");
-const pokemonCards = document.querySelectorAll(".card.hoverable");
-const pokemonModal = document.getElementById("pokemon-modal");
-
-// Tabs
-const ownedTabLink = document.getElementById("tab-owned-link");
-const wildTabLink = document.getElementById("tab-wild-link");
-const ownedTab = document.getElementById("owned-pokemon");
-const wildTab = document.getElementById("wild-pokemon");
-
 // Remove '.active' from the current active one.
 function deactiveCurrentTabLink() {
 	const activeTab = document.querySelector(".tabs__list__item.active");
@@ -15,83 +5,6 @@ function deactiveCurrentTabLink() {
 		activeTab.classList.remove("active");
 	}
 }
-
-// Open wild tab
-wildTabLink.addEventListener("click", () => {
-	wildTab.classList.add("show");
-	ownedTab.classList.remove("show");
-	deactiveCurrentTabLink();
-	wildTabLink.parentNode.classList.add("active");
-});
-
-// Open owned tab
-ownedTabLink.addEventListener("click", () => {
-	ownedTab.classList.add("show");
-	wildTab.classList.remove("show");
-
-	deactiveCurrentTabLink();
-	ownedTabLink.parentNode.classList.add("active");
-});
-
-// Handle back buttons
-backToPokemonModalButton.addEventListener("click", (e) => {
-	closeAllModals();
-	pokemonModal.classList.add("show");
-});
-
-// Open Pokemon Modal
-pokemonCards.forEach((element) => {
-	element.addEventListener("click", (e) => {
-		pokemonModal.classList.add("show");
-	});
-});
-
-// Open Use items Modal
-const openUseItemsModalButton = document.getElementById("open-use-items-modal-btn");
-const useItemsModal = document.getElementById("use-items-modal");
-openUseItemsModalButton.addEventListener("click", (e) => {
-	closeAllModals();
-	useItemsModal.classList.add("show");
-});
-
-// Show content for items
-const openConsumableBtn = document.getElementById("open-consumable-items");
-const openMysticBtn = document.getElementById("open-mystic-items");
-const consumableItemsContainer = document.getElementById("consumable-items");
-const mysticItemsContainer = document.getElementById("mystic-items");
-
-openConsumableBtn.addEventListener("click", (e) => {
-	consumableItemsContainer.classList.add("show");
-	openConsumableBtn.parentElement.classList.add("active");
-
-	mysticItemsContainer.classList.remove("show");
-	openMysticBtn.parentElement.classList.remove("active");
-});
-
-openMysticBtn.addEventListener("click", (e) => {
-	mysticItemsContainer.classList.add("show");
-	openMysticBtn.parentElement.classList.add("active");
-
-	consumableItemsContainer.classList.remove("show");
-	openConsumableBtn.parentElement.classList.remove("active");
-});
-
-// Select items
-const itemInputs = document.querySelectorAll("input.item-amount");
-
-itemInputs.forEach((itemInput) => {
-	itemInput.addEventListener("change", (e) => {
-		const { value, id } = e.target;
-		const containerId = `item-${id.split("-amount")[0]}`;
-		const containerElement = document.getElementById(containerId);
-
-		if (value > 0) {
-			containerElement.classList.add("selected");
-		} else {
-			containerElement.classList.remove("selected");
-		}
-	});
-});
 
 // ==== CREATE ELEMENTS ====
 function createPokemonCard(pokemon) {
@@ -106,8 +19,8 @@ function createPokemonCard(pokemon) {
 	const imgContainer = document.createElement("div");
 	imgContainer.className = "card__pokemon_img";
 	const pokemonImg = document.createElement("img");
-	// pokemonImg.src = `../../dist/img/${pokemon.imgName}`;
-	pokemonImg.src = `../../dist/img/charmander.svg`;
+	pokemonImg.src = `../../dist/img/pokemon/${pokemon.imgName}`;
+	// pokemonImg.src = `../../dist/img/charmander.svg`;
 	pokemonImg.alt = pokemon.name;
 	imgContainer.appendChild(pokemonImg);
 
@@ -150,7 +63,7 @@ function createPokemonCard(pokemon) {
 	tabItemElement.id = pokemon.id;
 	tabItemElement.appendChild(cardElement);
 
-    return tabItemElement
+	return tabItemElement;
 }
 
 async function main() {
@@ -173,6 +86,111 @@ async function main() {
 	ownedPokemon.forEach((pokemon) => {
 		const pokemonCardElement = createPokemonCard(pokemon);
 		ownedPokemonContainer.appendChild(pokemonCardElement);
+	});
+
+	const wildPokemonContainer = document.getElementById("wild-pokemon");
+	wildPokemon.forEach((pokemon) => {
+		const pokemonCardElement = createPokemonCard(pokemon);
+		wildPokemonContainer.appendChild(pokemonCardElement);
+	});
+
+	// ====== Event Handlers ======
+	const backToPokemonModalButton = document.getElementById("back-to-pokemon-modal-btn");
+	const pokemonCards = document.querySelectorAll(".card.hoverable");
+	const pokemonModal = document.getElementById("pokemon-modal");
+
+	// Tabs
+	const ownedTabLink = document.getElementById("tab-owned-link");
+	const wildTabLink = document.getElementById("tab-wild-link");
+	const ownedTab = document.getElementById("owned-pokemon");
+	const wildTab = document.getElementById("wild-pokemon");
+	const pokemonAmount = document.getElementById("amount");
+
+	// Show the total number of Pokemon
+	if (wildTabLink.parentNode.classList.contains("active")) {
+		pokemonAmount.textContent = wildPokemon.length;
+	}
+	if (ownedTabLink.parentNode.classList.contains("active")) {
+		pokemonAmount.textContent = ownedPokemon.length;
+	}
+
+	// Open wild tab
+	wildTabLink.addEventListener("click", () => {
+		wildTab.classList.add("show");
+		ownedTab.classList.remove("show");
+		deactiveCurrentTabLink();
+		wildTabLink.parentNode.classList.add("active");
+		pokemonAmount.textContent = wildPokemon.length;
+	});
+
+	// Open owned tab
+	ownedTabLink.addEventListener("click", () => {
+		ownedTab.classList.add("show");
+		wildTab.classList.remove("show");
+
+		deactiveCurrentTabLink();
+		ownedTabLink.parentNode.classList.add("active");
+		pokemonAmount.textContent = ownedPokemon.length;
+	});
+
+	// Handle back buttons
+	backToPokemonModalButton.addEventListener("click", (e) => {
+		closeAllModals();
+		pokemonModal.classList.add("show");
+	});
+
+	// Open Pokemon Modal
+	pokemonCards.forEach((element) => {
+		element.addEventListener("click", (e) => {
+			pokemonModal.classList.add("show");
+		});
+	});
+
+	// Open Use items Modal
+	const openUseItemsModalButton = document.getElementById("open-use-items-modal-btn");
+	const useItemsModal = document.getElementById("use-items-modal");
+	openUseItemsModalButton.addEventListener("click", (e) => {
+		closeAllModals();
+		useItemsModal.classList.add("show");
+	});
+
+	// Show content for items
+	const openConsumableBtn = document.getElementById("open-consumable-items");
+	const openMysticBtn = document.getElementById("open-mystic-items");
+	const consumableItemsContainer = document.getElementById("consumable-items");
+	const mysticItemsContainer = document.getElementById("mystic-items");
+
+	openConsumableBtn.addEventListener("click", (e) => {
+		consumableItemsContainer.classList.add("show");
+		openConsumableBtn.parentElement.classList.add("active");
+
+		mysticItemsContainer.classList.remove("show");
+		openMysticBtn.parentElement.classList.remove("active");
+	});
+
+	openMysticBtn.addEventListener("click", (e) => {
+		mysticItemsContainer.classList.add("show");
+		openMysticBtn.parentElement.classList.add("active");
+
+		consumableItemsContainer.classList.remove("show");
+		openConsumableBtn.parentElement.classList.remove("active");
+	});
+
+	// Select items
+	const itemInputs = document.querySelectorAll("input.item-amount");
+
+	itemInputs.forEach((itemInput) => {
+		itemInput.addEventListener("change", (e) => {
+			const { value, id } = e.target;
+			const containerId = `item-${id.split("-amount")[0]}`;
+			const containerElement = document.getElementById(containerId);
+
+			if (value > 0) {
+				containerElement.classList.add("selected");
+			} else {
+				containerElement.classList.remove("selected");
+			}
+		});
 	});
 }
 
