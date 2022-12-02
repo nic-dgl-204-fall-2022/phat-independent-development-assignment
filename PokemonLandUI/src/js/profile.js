18cf9808581858b1f728cf614ed480d73170cdbb
phat@minim1 <phat.tranducvn@gmail.com>
2022-12-01 17:37:13 -0800

    Check logged in status for profile page

--- PokemonLandUI/src/js/profile.js
+++ PokemonLandUI/src/js/profile.js
@@ -1,12 +1,14 @@
 const logoutBtn = document.getElementById("logout-btn");
+logoutBtn.addEventListener("click", () => {
+	setJwtToken(null);
+	redirectTo(CLIENT_PAGES.loginPage);
+});
 
-logoutBtn.addEventListener('click', () => {
-    setJwtToken(null)
-    redirectTo(CLIENT_PAGES.loginPage)
-})
 
 function main() {
-
+	const loggedIn = isLoggedIn();
+	if (!loggedIn) {
+        redirectTo(CLIENT_PAGES.loginPage);
+	}
 }
-
-main() 
+main();
